7f8177a4eaae37e4621792faaa229137cabf246c
Nicholas <nicholasang.sg@gmail.com>
2025-09-23 14:37:50 +0800

    shifting c++ code, from monolith app using enscripten to client-server architecure through websockets

---
+++ server/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Include the iostream library for input/output operations
+using namespace std;
+
+int main() { // The main function, where program execution begins
+    cout << "Hello World!" << endl; // Print "Hello World!" to the console
+    return 0; // Indicate successful program execution
+}
